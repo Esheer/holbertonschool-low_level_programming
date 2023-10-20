021beefd0e19e4c1234fd626caf808916faf3c7b
Esheer <fabcis19@gmail.com>
2023-10-20 10:54:46 -0700

    Escriba un programa que imprima todas las combinaciones posibles de números de un solo dígito

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {       
-	int digit = 0;
-	while (digit < 10)
-	{
-		putchar (digit + '0');
-	if (digit < 9)
-        {
-		putchar (',');
-		putchar (' ');
-	}
-                digit++;
-	}
-        putchar('\n');
+	      int digit = 0;
+	      while (digit < 10)
+              {
+	      putchar (digit + '0');
+	      if (digit < 9)
+              {
+	      putchar (',');
+              putchar (' ');
+	      }
+              digit++;
+	      }
+              putchar('\n');
 
 	return (0);
 }	
