82ad53848cdec390f205bc12e4a1561494046594
Esheer <fabcis19@gmail.com>
2023-10-20 23:36:53 -0700

    Escriba un programa que imprima todos los números de un solo dígito en base 10 comenzando desde 0, seguido de una nueva línea

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Print all single digit numbers
+ *
+ * Return: Aways 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d" , i);
+	}
+	putchar ('\n');
+
+	return (0);
+}
