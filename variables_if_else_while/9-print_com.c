b23bc16371c22728cf0c099c248b67bce20bd9b1
Esheer <fabcis19@gmail.com>
2023-10-21 16:26:12 -0700

    Escriba un programa que imprima todas las combinaciones posibles de números de un solo dígito. Los números deben estar separados por ,, seguido de un espacio

--- variables_if_else_while/9-print_com.c
+++ variables_if_else_while/9-print_com.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - print prints all single digit numerbs of base 10 starting from 0
+ * main - Print prints all single digit numbers of base 10 starting from 0
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int digit = 0;
-	
-	while (digit < 10)
-	{
-		putchar(digit + '0');
-		if (digit < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		digit++;
-	}
-	putchar('\n');
+    int digit = 0;
 
-	return (0);
+    while (digit < 10)
+    {
+        putchar(digit + '0');
+        if (digit < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+        digit++;
+    }
+    putchar('\n');
+
+    return (0);
 }
