2885c5f858f308d0999422781a47c0ebab16dbc9
Esheer <fabcis19@gmail.com>
2023-10-21 14:28:19 -0700

    Escribe un programa que imprima todos los números de base 16 en minúsculas, seguido de una nueva línea

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *  main - Print the numers numerbs of base 16 in lowercase
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char a;
+	
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	
+	}
+	
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
