93c4041da04007bc81ba3bb917e622a4ca1cd30a
Esheer <fabcis19@gmail.com>
2023-10-20 19:44:50 -0700

    Escriba un programa que imprima el alfabeto en minúsculas, seguido de una nueva línea

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,16 @@
  * Return: Always (Success)
  */
 
-int main (void)
+int main(void)
 
 {
-	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
-	{	
-		putchar (c);
-	}	
-	        putchar ('\n');
+    char c;
 
-        return (0);
+    for (c = 'a'; c <= 'z'; c++)
+    {
+	  putchar (c);
+    }	
+    putchar ('\n');	
+
+    return (0);
 }
