591361db0832677a9547c4b73f5109f976253ea3
Esheer <fabcis19@gmail.com>
2023-10-22 14:44:52 -0700

    Escribe una función que compruebe si hay caracteres en minúscula. Prototipo: int _islower(int c); Devuelve 1 si c es minúscula Devuelve 0 en caso contrario

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - returns 1 if lowercase alphabet and 0 otherwise
+ * @c: the character to be cheked
  * _islower takes in a character
  * Return: 0 for uppercase, 1 for lowercase
  */
