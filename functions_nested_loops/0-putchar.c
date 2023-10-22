9d659b88fea8e0f1037e5b1faa39238d86fe893d
Esheer <fabcis19@gmail.com>
2023-10-21 19:13:22 -0700

    escribir putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _putchar - Write a program that prints _putchar, followed by a new line
+ * 
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	
+	return (0);
+}
