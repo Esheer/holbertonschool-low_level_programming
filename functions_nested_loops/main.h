448c52f08567a7ee9aaab1ddb43c2ce761b4ed05
Esheer <fabcis19@gmail.com>
2023-10-22 13:34:31 -0700

    hola

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Prototipo de la función _putchar */
 int _putchar(char c);
 
-#endif
+/* Prototipo de la función print_alphabet */
+void print_alphabet(void);
 
+#endif /* MAIN_H */
