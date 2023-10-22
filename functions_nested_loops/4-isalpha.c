de7e352e4f14ef1fabc6336e267e4c21371ddfc9
Esheer <fabcis19@gmail.com>
2023-10-22 14:52:27 -0700

    Escribe una función que verifique los caracteres alfabéticos. Prototipo: int _isalpha(int c); Devuelve 1 si c es una letra, minúscula o mayúscula Devuelve 0 en caso contrario

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,19 +2,19 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: the character to be cheked 
+ * @c: the character to be checked
  * _isalpha takes in a character
- * Return: 1 if letter, lowercase, uppercase; 0 for otherwise
+ * Return: 1 if letter, lowercase, uppercase; 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
