9e2db539885717ee759025571ecb9b170639a0ad
Esheer <fabcis19@gmail.com>
2023-11-03 09:50:09 -0700

    Defina un nuevo tipo struct dog con los siguientes elementos

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct god - realizar definiciones de tipo
+ * @name : definicion nombre
+ * @owner : definicion de dueño
+ * @age : definicion de edad
+ * description: definicion de los tipos de struct 
+ *
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+		
