7411164c438fb852ae86d0506b9a5ee38796c798
Esheer <fabcis19@gmail.com>
2023-11-29 17:42:34 -0800

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - prints a string
- * followed by a new line, to stdout
- * @str: string
- *
- * Return: void
+ *_puts - prints a string, to stdout
+ *@str: value to be evaluate.
+ *Return: not.
  */
 
 void _puts(char *str)
 {
-	while (*str)
+	while (*str != '\0')
+	{
 		_putchar(*str++);
-
-	_putchar('\n');
+	}
+		_putchar('\n');
 }
